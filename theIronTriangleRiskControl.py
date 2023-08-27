f81cc7e16e710812cc40556d36183a767503a652
emidwate <matthewcode04@gmail.com>
2023-08-27 13:09:31 +0200

    Update v0.1

--- theIronTriangleRiskControl.py
+++ theIronTriangleRiskControl.py
@@ -1,7 +1,7 @@
 capital = float(input("Enter capital in $/Zl: "))
-percentage = float(input("Enter percentage risk, less than 2%: "))
-result = capital * percentage / 100
-print("Max lost you can afford which is the percentage you pasted according to the method:", result)
+percentage = float(input("Enter percentage of risk you can afford to loose in one transaction in percentage %: "))
+firstArmOfTriangle = capital * percentage / 100
+print("Max lost you can afford which is the percentage you pasted above:", firstArmOfTriangle)
 
 while True:
     value = input("Buy or Sell: ")
@@ -11,34 +11,50 @@ while True:
             print("Invalid input. Please enter either 'Buy' or 'Sell'.")
             x = input("Buy or Sell: ")
         if x == "Buy" or x == "buy":
-            enter = float(input("Enter the position rate: "))
-            takeProfit = float(input("Enter take profit value: "))
-            stopLoss = float(input("Enter stop loss value: "))
-            score = enter - stopLoss
-            devide = (takeProfit - enter) / score
+            while True:
+                try:
+                    enter = float(input("Enter the position rate: "))
+                    takeProfit = float(input("Enter take profit value: "))
+                    stopLoss = float(input("Enter stop loss value: "))
+        
+                    lostMoneyPerShare = enter - stopLoss
+                    earnMoneyPerShare = takeProfit - enter
+                    riskRatio = earnMoneyPerShare / lostMoneyPerShare
+        
+                    break
+
+                except ValueError:
+                    print("One or more inputs are not valid numbers. Please try again.")
         if x == "Sell" or x == "sell":
-            enter = float(input("Enter the position rate: "))
-            takeProfit = float(input("Enter take profit value: "))
-            stopLoss = float(input("Enter stop loss value: "))
-            score = stopLoss - enter
-            devide = (enter - takeProfit) / score
+            while True:
+                try:
+                    enter = float(input("Enter the position rate: "))
+                    takeProfit = float(input("Enter take profit value: "))
+                    stopLoss = float(input("Enter stop loss value: "))
 
-        maxShares = int(capital // enter)
-        outcome = int(result // score)
-        maxOutcome = min(outcome, maxShares)
+                    lostMoneyPerShare = stopLoss - enter
+                    earnMoneyPerShare = takeProfit - enter
+                    riskRatio = earnMoneyPerShare / lostMoneyPerShare
+                    
+                    break
 
-        if maxOutcome == 0:
+                except ValueError:
+                    print("One or more inputs are not valid numbers. Please try again.")
+        secondArmOfTriangle = int(firstArmOfTriangle // lostMoneyPerShare)
+
+        if secondArmOfTriangle == 0:
             print("You cannot afford any shares with the given parameters")
         else:
-            print("The maximum amount of shares:", maxOutcome)
-            print("The maximum price of transaction in $/Zl:", maxOutcome * enter)
-            print("Risk ratio:", devide)
-            print("Possible to earn:", abs(maxOutcome * (takeProfit - enter)))
-            print("Possible to lose:", abs(maxOutcome * (enter - stopLoss)))
-            print('Left capital after buying max:', capital - (maxOutcome * enter))
+
+          print("The maximum amount of shares:", secondArmOfTriangle)
+          print("The maximum price of transaction in $/Zl:", secondArmOfTriangle * enter)
+          print("Profit/Risk ratio:", abs(riskRatio))
+          print("Possible to earn:", abs(secondArmOfTriangle * (takeProfit - enter)))
+          print("Possible to lose:", abs(secondArmOfTriangle * (enter - stopLoss)))
+          print('Left capital after buying max amount of shares:', capital - (secondArmOfTriangle * enter))
 
     fun(value)
-    repeat = input("Do you want to run the program again? (yes/no) ")
+    repeat = input("Do you want to run the program again? (yes/no): ")
     if repeat.lower() != "yes":
         break
 
